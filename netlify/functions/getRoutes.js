9d4d2f33bdd46cb878ae313beb0c34a4e8a2ba17
Kaló, Róbert <kalo.robi@gmail.com>
2026-03-19 14:07:43 +0100

    Change Supabase table reference to 'public.routes'

--- netlify/functions/getRoutes.js
+++ netlify/functions/getRoutes.js
@@ -7,7 +7,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 exports.handler = async (event, context) => {
   // opcionális: auth, query paraméterek stb.
   const { data, error } = await supabase
-    .from('routes')
+    .from('public.routes')
     .select('*');
 
   if (error) {
