9097fc274b049dc0aa784cd29f2169079684dbee
Kaló, Róbert <kalo.robi@gmail.com>
2026-03-19 13:58:38 +0100

    Supabase test

---
+++ netlify/functions/getRoutes.js
@@ -0,0 +1,24 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+exports.handler = async (event, context) => {
+  // opcionális: auth, query paraméterek stb.
+  const { data, error } = await supabase
+    .from('routes')
+    .select('*');
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
